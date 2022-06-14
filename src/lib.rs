1cf12a5fb0310f7e60ab6f4900f49519f7ad2120
Seokju Na <seokju.me@toss.im>
2022-06-15 01:04:40 +0900

    juso example

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,107 @@
-use serde_json::json;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 use worker::*;
 
 mod utils;
 
 fn log_request(req: &Request) {
-    console_log!(
-        "{} - [{}], located at: {:?}, within: {}",
-        Date::now().to_string(),
-        req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or("unknown region".into())
-    );
+  console_log!(
+    "{} - [{}], located at: {:?}, within: {}",
+    Date::now().to_string(),
+    req.path(),
+    req.cf().coordinates().unwrap_or_default(),
+    req.cf().region().unwrap_or_else(|| "unknown region".into())
+  );
 }
 
 #[event(fetch)]
-pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
-    log_request(&req);
+pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+  log_request(&req);
+  utils::set_panic_hook();
 
-    // Optionally, get more helpful error messages written to the console in the case of a panic.
-    utils::set_panic_hook();
+  let router = Router::new();
+  router
+    .post_async("/search", search_juso)
+    .run(req, env)
+    .await
+}
 
-    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
-    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
-    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
-    let router = Router::new();
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct PlaceDocument {
+  pub id: String,
+  pub place_name: String,
+  pub category_name: String,
+  pub category_group_code: String,
+  pub category_group_name: String,
+  pub phone: String,
+  pub address_name: String,
+  pub road_address_name: String,
+  pub x: String,
+  pub y: String,
+  pub place_url: String,
+  pub distance: String,
+}
 
-    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
-    // functionality and a `RouteContext` which you can use to  and get route parameters and
-    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
-    router
-        .get("/", |_, _| Response::ok("Hello from Workers!"))
-        .post_async("/form/:field", |mut req, ctx| async move {
-            if let Some(name) = ctx.param("field") {
-                let form = req.form_data().await?;
-                match form.get(name) {
-                    Some(FormEntry::Field(value)) => {
-                        return Response::from_json(&json!({ name: value }))
-                    }
-                    Some(FormEntry::File(_)) => {
-                        return Response::error("`field` param in form shouldn't be a File", 422);
-                    }
-                    None => return Response::error("Bad Request", 400),
-                }
-            }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct PlaceSearchMetadata {
+  pub total_count: i32,
+  pub pageable_count: i32,
+  pub is_end: bool,
+}
 
-            Response::error("Bad Request", 400)
-        })
-        .get("/worker-version", |_, ctx| {
-            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
-            Response::ok(version)
-        })
-        .run(req, env)
-        .await
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct PlaceSearchResult {
+  pub meta: PlaceSearchMetadata,
+  pub documents: Vec<PlaceDocument>,
+}
+
+async fn search_juso(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+  let keyword = req
+    .url()?
+    .query_pairs()
+    .find_map(|(key, value)| {
+      if key == "keyword" {
+        return Some(value.to_string());
+      }
+      None
+    })
+    .unwrap_or_default();
+
+  // 캐시가 있다면 반환
+  let cache = ctx.kv("JUSO_CACHE")?;
+  if let Ok(Some(result)) = cache.get(&keyword).json::<PlaceSearchResult>().await {
+    console_log!("hit cache!: {}", keyword);
+    return Response::from_json(&result);
+  }
+
+  // 카카오API 호출
+  let kakao_api_key = ctx.secret("KAKAO_API_KEY")?.to_string();
+  let mut headers = Headers::new();
+  headers.append("Authorization", &format!("KakaoAK {}", kakao_api_key))?;
+
+  let mut req_init = RequestInit::new();
+  req_init.with_method(Method::Post).with_headers(headers);
+
+  let mut req_url = Url::from_str("https://dapi.kakao.com/v2/local/search/keyword.json")?;
+  let query = "query=".to_owned() + &keyword;
+  req_url.set_query(Some(&query));
+
+  let req = Request::new_with_init(req_url.as_str(), &req_init)?;
+  let mut res = Fetch::Request(req).send().await?;
+
+  match res.status_code() {
+    200 => {
+      // 결과값을 캐시에 저장
+      let result = res.json::<PlaceSearchResult>().await?;
+      console_log!("missed cache: {}", keyword);
+      cache
+        .put(&keyword, &result)?
+        .expiration_ttl(60)
+        .execute()
+        .await?;
+
+      Response::from_json(&result)
+    }
+    _ => Ok(res),
+  }
 }
